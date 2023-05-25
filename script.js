7a6b9a5bc42ec73f3c77c55eef3d65738c17ac22
Mubashar <muhammadmubashar596@gmail.com>
2023-05-25 01:34:47 -0700

    fist commit

---
+++ script.js
@@ -0,0 +1,107 @@
+// var mubashar = 50
+// mubashar = 250
+// console.log(mubashar);
+
+
+// var asad1 = 5
+// var asad2 = 560
+// var result = asad1 + asad2
+// console.log(result);
+
+// var asad1 = prompt("please enter your number");
+// var asad2 = prompt("please enter your number 2");
+// var result = asad1 + asad2
+
+// console.log(result);
+
+
+// function myFunction() {
+//     let person = prompt("Please enter your name", "Harry Potter");
+//     if (person != null) {
+//       document.getElementById("demo").innerHTML =
+//       "Welcome " + person + " !how can i help you?";
+//     }
+//   }
+
+
+// var equal1 = prompt("enter your number" ,"Enter your Marks")
+// var equal2 =  1100
+// var equal3 = 100
+// var result = equal1 / equal2 * equal3
+// if (result!= null) {
+//     document.getElementById("demo").innerHTML =
+//      result;
+    
+// }
+
+// console.log(result);
+
+
+// var marks = prompt("Enter Your number check Grade","1....100")
+
+// if (marks < 50) {
+//     document.getElementById("demo").innerHTML=("Student is Fail")
+// }
+// else if (marks >=50 && marks < 60) {
+//     document.getElementById("demo").innerHTML=("Student Grade is D")
+// }
+// else if (marks>=60 &&marks<70 ) {
+//     document.getElementById("demo").innerHTML=("Student Grade is C")
+// }
+// else if (marks>=70 &&marks<80 ) {
+//     document.getElementById("demo").innerHTML=("Student Grade is B")
+// }
+// else if (marks>=80 &&marks<90 ) {
+//     document.getElementById("demo").innerHTML=("Student Grade is A")
+// }
+// else if (marks>=90 &&marks<100 ) {
+//     document.getElementById("demo").innerHTML=("Student Grade is A+")
+// }
+
+
+
+// var names = ["Naveed", "asad", "umer", "weldone"];
+
+// console.log("welcome",names[3]);
+
+
+
+// var owner = prompt("Enter your Name")
+
+// document.getElementById("demo").innerHTML= ("Welcome To Back Mr !" + owner);
+
+
+function myFunction() {
+    var x = document.createElement ("text");
+    var t = document.createTextNode("ok")
+    x.appendChild(t);
+    document.body.appendChild(x);
+    
+}
+
+
+
+
+
+
+// function myFunction(){
+//     var mubashar=10
+//     var asad= prompt("Enter number")
+//     // let result= (mubashar)+(asad);
+//     function searchURL() {
+//         var.location= mubashar+asad
+        
+//     }
+
+
+
+
+
+
+//     document.getElementById("demo").innerHTML=(result);
+
+// var mubashar=10
+// var asad= prompt("Enter number")
+// var result= (mubashar)+(asad);
+// function searchURL() 
+//     var result= mubashar+asad
